38d060d3160c879961ddd8e060ba17b1d23cc368
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2019-02-19 18:48:34 +0000

    added route for roman num conversion added controller for roman num conversion

---
+++ src/controllers/roman.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "restify";
+import * as model from "../models/Roman";
+
+export const getNumber = async (req: Request, res: Response) => {
+    const { number } = req.params;
+
+    if (isNaN(number)) {
+        res.send(400);
+    }
+
+    res.send(200, {
+        inputValue: number,
+        convertedNum: await model.toRoman(number)
+    });
+};
--- src/routes/roman.ts
+++ src/routes/roman.ts
@@ -1,7 +1,8 @@
 import { Router } from "restify-router";
+import { getNumber } from "../controllers/roman";
 
 const romanRoutes = new Router();
 
-
+romanRoutes.get("/roman/:number", getNumber);
 
 export default romanRoutes; 
