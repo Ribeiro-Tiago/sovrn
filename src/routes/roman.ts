2f905178a09a53da1f0ba7de6635eab37442e0bd
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2019-02-22 18:26:23 +0000

    Delete roman.ts

--- src/routes/roman.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "restify-router";
-
-import { getNumber } from "../controllers/Roman";
-
-const romanRoutes = new Router();
-
-romanRoutes.get("/roman/:number", getNumber);
-
-export default romanRoutes; 
