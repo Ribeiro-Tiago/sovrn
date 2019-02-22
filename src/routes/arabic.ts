4bd43050348f53327c7b69719097a835cd8c3a6a
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2019-02-22 18:26:16 +0000

    Delete arabic.ts

--- src/routes/arabic.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "restify-router";
-
-import { getNumber } from "../controllers/Arabic";
-
-const arabicRoutes = new Router();
-
-arabicRoutes.get("/arabic/:number", getNumber);
-
-export default arabicRoutes;
