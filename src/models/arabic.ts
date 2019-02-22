d72a4f76bb13ba43d7eda7c56cf4f7098e9d51fa
Tiago Ribeiro <tiagoribeiro.1803@gmail.com>
2019-02-22 18:26:01 +0000

    Delete arabic.ts

--- src/models/arabic.ts
+++
@@ -1,8 +0,0 @@
-import { getValue } from "../utils/Finder";
-
-/**
- * Looks for the number we want to convert in the db. If we find it, we return it. If not we convert, store on db and return the converted value.
- * @param {number} inputValue - num we want to convert
- * @return {Promise<string>} a promise that resovles to the converted number
- */
-export const toArabic = async (inputValue: number): Promise<string> => getValue(inputValue, "arabics") as Promise<string>;
